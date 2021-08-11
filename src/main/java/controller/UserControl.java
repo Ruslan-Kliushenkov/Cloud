74fca6df1d215d1be791fd9037c16c57dcdc5259
Ruslan-Kliushenkov <ruslan.klyshenkov@gmail.com>
2021-08-12 01:28:14 +0300

    "successfully"

--- src/main/java/controller/UserControl.java
+++ src/main/java/controller/UserControl.java
@@ -1,9 +1,6 @@
 package controller;
 
 import lombok.SneakyThrows;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
